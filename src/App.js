eedaac8a0bc830de742a35e9b760fa7af93407eb
DylanLovell <arcanyne7@gmail.com>
2021-11-19 10:49:53 -0500

    creating components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import "./App.css";
+import { Component } from 'react';
+import './App.css';
 
-const App = () => {
-  return <h1>Hello, world!</h1>;
-};
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cart: [],
+    };
+  }
+
+  render() {
+    return <h1>Hello, world!</h1>;
+  }
+}
 
 export default App;
