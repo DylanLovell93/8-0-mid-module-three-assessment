121d3c70cb10328a45788eaa71e3d66390827d13
DylanLovell <arcanyne7@gmail.com>
2021-11-19 15:38:57 -0500

    created sortData.js

---
+++ src/data/sortData.js
@@ -0,0 +1,24 @@
+const sortData = [
+  {
+    title: '---',
+    value: '',
+  },
+  {
+    title: 'Low to High (Ascending)',
+    value: 'lowHigh',
+  },
+  {
+    title: 'High to Low (Descending)',
+    value: 'highLow',
+  },
+  {
+    title: 'Alphabetical (Ascending)',
+    value: 'aZ',
+  },
+  {
+    title: 'Alphabetical (Descending)',
+    value: 'zA',
+  },
+];
+
+export default sortData;
